234aff5d283f31fed8ec041309bf344c8b694335
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-06-30 16:04:50 +0200

    i18n

---
+++ 00-i18n.sh
@@ -0,0 +1,8 @@
+export LANG=en_GB.UTF-8
+export LC_NUMERIC=sv_SE.UTF-8
+export LC_TIME=sv_SE.UTF-8
+export LC_COLLATE=sv_SE.UTF-8
+export LC_MONETARY=sv_SE.UTF-8
+export LC_NAME=sv_SE.UTF-8
+export LC_TELEPHONE=sv_SE.UTF-8
+export LC_MEASUREMENT=sv_SE.UTF-8
