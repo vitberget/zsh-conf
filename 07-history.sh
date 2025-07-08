034c0886d37f241764cbcb683e02a79586510a22
Kenneth Hedman <github@vitberget.se>
2025-07-08 07:05:36 +0200

    dont save history to file if line end with # NO HISTFILE

--- 07-history.sh
+++ 07-history.sh
@@ -2,3 +2,19 @@ HISTSIZE=10000
 SAVEHIST=10000
 HISTFILE=~/.cache/zsh-history
 setopt HIST_IGNORE_SPACE
+setopt interactivecomments
+
+function no_history_with_this_comment() {
+ if [[ $1 == *'# NO HISTFILE' ]]; then
+     return 1
+ fi
+}
+
+add-zsh-hook zshaddhistory no_history_with_this_comment
+
+addNoComment() {
+    RBUFFER="$RBUFFER # NO HISTFILE"
+}
+
+zle -N addNoComment
+bindkey '^[^Z' addNoComment
--- 68-insert-before.sh
+++ 68-insert-before.sh
@@ -1,13 +1,6 @@
-# addDevour() {
-#     LBUFFER="devour $LBUFFER"
-# }
-
 addSudo() {
     LBUFFER="sudo $LBUFFER"
 }
 
 zle -N addSudo
-# zle -N addDevour
-
 bindkey '^[^X' addSudo
-# bindkey '^[^Z' addDevour
