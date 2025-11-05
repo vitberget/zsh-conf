ab129f5aab2f8feb773038c217087e581f57f056
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-11-05 13:54:11 +0100

    nvm

--- 92-nvm.sh
+++ 92-nvm.sh
@@ -1,5 +1,5 @@
 if [ -f /usr/share/nvm/init-nvm.sh ]; then
     alias init-nvm='source /usr/share/nvm/init-nvm.sh'
-elif [ -f "$NVM_DIR/nvm.sh"]; then
+elif [ -f "$NVM_DIR/nvm.sh" ]; then
     alias init-nvm='source $NVM_DIR/nvm.sh'
 fi
