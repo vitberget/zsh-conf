fdf2875c1dfe61ac743d9a1db399dbc181e30d54
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-11-27 09:36:49 +0100

    better kube-namespace

--- 04-kubectl.sh
+++ 04-kubectl.sh
@@ -1,9 +1,13 @@
-alias kc='kubectl'
-
 function kube-namespace() {
     if [ -n "$1" ]; then
         kubectl config set-context --current --namespace="$1"
     else
-        kubectl get namespace | grep Active | awk '{ print $1 }'
+        NAMESPACE=$(kubectl get namespace | grep Active | awk '{ print $1 }' | fzf 2>/dev/null)
+        if [ -n "$NAMESPACE" ]; then
+            kubectl config set-context --current --namespace="$NAMESPACE"
+        fi
     fi
 }
+
+alias kc='kubectl'
+alias kc-namespace=kube-namespace
