96438382d68cecd852732e448d88e09d61b91f0e
Kenneth Hedman <kenneth.hedman@firefly.se>
2026-01-07 16:11:54 +0100

    run init-nvm when folder contains .nvmrc and is not previously loaded

--- 92-nvm.sh
+++ 92-nvm.sh
@@ -3,3 +3,13 @@ if [ -f /usr/share/nvm/init-nvm.sh ]; then
 elif [ -f "$NVM_DIR/nvm.sh" ]; then
     alias init-nvm='source $NVM_DIR/nvm.sh'
 fi
+
+function chpwd_init_nvm_on_nvmrc() {
+    if [[ -f ".nvmrc" ]]; then
+        if ! whence nvm >/dev/null 2>/dev/null ; then
+            init-nvm
+        fi
+    fi
+}
+
+chpwd_functions+=(chpwd_init_nvm_on_nvmrc)
