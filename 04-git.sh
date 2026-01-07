6270d2df40bf70afc5145d0a057fea5ce2a43388
Kenneth Hedman <kenneth.hedman@firefly.se>
2026-01-07 16:27:54 +0100

    git keybindds

---
+++ 04-git.sh
@@ -0,0 +1,9 @@
+git_commit_with_message() {
+    LBUFFER="git commit -m \""
+    RBUFFER="\""
+}
+
+zle -N git_commit_with_message
+bindkey '^Xgc' git_commit_with_message
+
+bindkey -s '^Xgs' 'git status\n'
