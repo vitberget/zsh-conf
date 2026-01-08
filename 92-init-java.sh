92e1816b958081cd8bfec38938e44e7d509e61be
Kenneth Hedman <kenneth.hedman@firefly.se>
2026-01-08 17:06:40 +0100

    init-java 21 & 25

---
+++ 92-init-java.sh
@@ -0,0 +1,9 @@
+function init-java21() {
+    export PATH=/usr/lib/jvm/java-21-openjdk/bin/:$PATH
+    export JAVA_HOME=/usr/lib/jvm/java-21-openjdk/
+}
+
+function init-java25() {
+    export PATH=/usr/lib/jvm/java-25-openjdk/bin/:$PATH
+    export JAVA_HOME=/usr/lib/jvm/java-25-openjdk/
+}
--- 92-java21.sh
+++
@@ -1,4 +0,0 @@
-function init-java21() {
-    export PATH=/usr/lib/jvm/java-21-openjdk/bin/:$PATH
-    export JAVA_HOME=/usr/lib/jvm/java-21-openjdk/
-}
