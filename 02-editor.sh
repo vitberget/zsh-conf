3c046f71c1fe32a438d0f887e8960ab8037c44eb
Kenneth Hedman <kenneth.hedman@firefly.se>
2026-03-30 15:41:14 +0200

    nvim-bob

--- 02-editor.sh
+++ 02-editor.sh
@@ -9,3 +9,5 @@ alias vim='nvim'
 alias nvim-mini="NVIM_APPNAME=nvim-mini nvim"
 alias mini="NVIM_APPNAME=nvim-mini nvim"
 
+alias nvim-bob="NVIM_APPNAME=nvim-bob /home/kenneth/.local/share/bob/nvim-bin/nvim"
+
--- 03-path.sh
+++ 03-path.sh
@@ -1,11 +1,11 @@
 path=(
     "$HOME/bin"
-    "$HOME/.local/share/bob/nvim-bin"
     "$HOME/.local/bin"
     "$HOME/.cargo/bin"
     "$HOME/.ghcup/bin"
     "$HOME/.config/nvim/vim-compiler"
     "/usr/lib/rustup/bin"
     $path
+    "$HOME/.local/share/bob/nvim-bin"
 )
 export PATH
