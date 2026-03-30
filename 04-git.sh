1b79d64895f9ddf95ecc4e94124c9ab773083d38
Kenneth Hedman <kenneth.hedman@firefly.se>
2026-03-30 15:43:03 +0200

    git

--- 04-git.sh
+++ 04-git.sh
@@ -6,5 +6,6 @@ git_commit_with_message() {
 zle -N git_commit_with_message
 bindkey '^Xgc' git_commit_with_message
 
+bindkey -s '^Xgp' 'git push'
 bindkey -s '^Xgs' 'git status\n'
 bindkey -s '^Xga' 'git add '
