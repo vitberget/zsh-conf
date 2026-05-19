9e2a3b61116f764deba54fe1daaa1814ff75c637
Kenneth Hedman <github@vitberget.se>
2026-05-19 10:34:09 +0200

    openoffice

--- 87-openoffice.sh
+++ 87-openoffice.sh
@@ -3,4 +3,6 @@ if command -v libreoffice >/dev/null; then
     alias oo-impress="libreoffice --impress --nologo"
     alias oo-calc="libreoffice --calc --nologo"
     alias oo-draw="libreoffice --draw --nologo"
+    alias oo-math="libreoffice --math --nologo"
+    alias oo-base="libreoffice --base --nologo"
 fi
