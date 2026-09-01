72a21cc462617d8bd236e29458397af8d671432f
Kenneth Hedman <github@vitberget.se>
2026-09-01 12:52:32 +0200

    kube ns fix

--- 04-kubectl.sh
+++ 04-kubectl.sh
@@ -1,16 +1,5 @@
 function kube-namespace() {
     if [ -n "$1" ]; then
-        export KUBE_NS="$1"
-    else
-        NAMESPACE=$(kubectl get namespace | grep Active | awk '{ print $1 }' | fzf 2>/dev/null)
-        if [ -n "$NAMESPACE" ]; then
-            export KUBE_NS="$NAMESPACE"
-        fi
-    fi
-}
-
-function kube-ctx-namespace() {
-    if [ -n "$1" ]; then
         kubectl config set-context --current --namespace="$1"
     else
         NAMESPACE=$(kubectl get namespace | grep Active | awk '{ print $1 }' | fzf 2>/dev/null)
@@ -20,16 +9,7 @@ function kube-ctx-namespace() {
     fi
 }
 
-function kc() {
-    if [[ -v KUBE_NS ]]; then
-        kubectl -n $KUBE_NS "$@"
-    else
-        kubectl "$@"
-    fi
-}
-
-alias kc-namespace=kube-namespace
 alias kube-ns=kube-namespace
 alias kc-ns=kube-namespace
+alias kc=kubectl
 
-compdef _kubectl kc
